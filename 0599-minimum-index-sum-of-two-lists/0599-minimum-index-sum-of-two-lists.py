0390247fa0d0817513bfadaf4ec6722b3b39e19c
Firaol kefeni <firaolkef@gmail.com>
2026-02-05 14:50:40 +0300

    Time: 77 ms (32.27%), Space: 19.9 MB (15.53%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -3,10 +3,13 @@ class Solution:
         
         least = float("inf")
         arr = []
+        set_list2 = set(list2)
 
         for i in range(len(list1)):
             string = list1[i]
-
+            if string not in set_list2:
+                continue
+            
             for j in range(len(list2)):
                 if string == list2[j]:
                     if i+j > least:
