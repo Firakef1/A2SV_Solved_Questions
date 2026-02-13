0dde31791362939aa20205e0ad50d0f145957080
Firaol kefeni <firaolkef@gmail.com>
2026-02-13 17:37:53 +0300

    Add files via upload

---
+++ A_Beautiful_Matrix.py
@@ -0,0 +1,11 @@
+matrix = []
+
+for _ in range(5):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            print(abs(2-i)+abs(2-j))
+            break
