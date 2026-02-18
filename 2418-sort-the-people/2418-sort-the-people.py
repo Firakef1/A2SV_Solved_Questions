80ec091d1c9eaf8a6ccaea1c4ae9fde7100070f2
Firaol kefeni <firaolkef@gmail.com>
2026-02-18 20:03:41 +0300

    Time: 17 ms (26.44%), Space: 19.8 MB (42.31%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,11 +1,45 @@
 class Solution:
+    def radix_sort(self, arr):
+        if not arr:
+            return arr
+
+        output = arr.copy()
+        max_num = max(output)
+        exp = 1 
+
+        while max_num // exp > 0:
+            n = len(output)
+            count = [0] * 10
+            temp = [0] * n
+
+ 
+            for num in output:
+                digit = (num // exp) % 10
+                count[digit] += 1
+
+     
+            for i in range(1, 10):
+                count[i] += count[i - 1]
+
+  
+            for i in range(n - 1, -1, -1):
+                digit = (output[i] // exp) % 10
+                temp[count[digit] - 1] = output[i]
+                count[digit] -= 1
+
+            output = temp
+            exp *= 10
+
+        return output
+
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-        lis = list(zip(heights, names))
+        lis = dict(zip( heights, names))
 
-        lis.sort(key=lambda x: -x[0])
+        heights = self.radix_sort(heights)
 
-        for i in range(len(lis)):
-            names[i] = lis[i][1]
+        for i in range(len(heights)):
+            names[i] = lis[heights[i]]
 
-        return names
+        return names[::-1]
+        
