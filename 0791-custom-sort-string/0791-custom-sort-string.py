e28c49c7c095f054bafbcdf9be0afcfc9163d34b
Firaol kefeni <firaolkef@gmail.com>
2026-02-23 17:19:45 +0300

    Time: 0 ms (100%), Space: 19.3 MB (77.72%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        order_dict = {}
+        for i, n in enumerate(order):
+            order_dict[n] = i
+        def order(x):
+            if x in order_dict:
+                return order_dict[x]
+            return -1
+        s = sorted(s, key = lambda x: order(x))
+
+        s = "".join(s)
+        return s
+
+        
