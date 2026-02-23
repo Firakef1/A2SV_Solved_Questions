ccbfeedbd306caa7ead8f02aa223d8021ede6e73
Firaol kefeni <firaolkef@gmail.com>
2026-02-23 17:20:14 +0300

    Time: 0 ms (100%), Space: 19.3 MB (59.55%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -3,10 +3,13 @@ class Solution:
         order_dict = {}
         for i, n in enumerate(order):
             order_dict[n] = i
+
         def order(x):
             if x in order_dict:
                 return order_dict[x]
+            
             return -1
+        
         s = sorted(s, key = lambda x: order(x))
 
         s = "".join(s)
