4a379394e4caf9ab640bde96b8cbd4a885401e50
Firaol kefeni <firaolkef@gmail.com>
2026-03-12 08:50:51 +0300

    Time: 51 ms (77.31%), Space: 21 MB (83.08%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        arr = []
+
+        def dfs(node):
+            nonlocal arr
+            if not node:
+                return
+            
+            arr.append(node.val)
+
+            for child in node.children:
+                dfs(child)
+            
+        dfs(root)
+
+        return arr        
