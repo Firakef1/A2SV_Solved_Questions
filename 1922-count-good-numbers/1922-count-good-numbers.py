b145ab86fe01b75e00c90ed02e1a66654ca5bc8e
Firaol kefeni <firaolkef@gmail.com>
2026-03-17 18:34:46 +0300

    Time: 0 ms (100%), Space: 19.1 MB (97.68%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,15 +1,9 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        # if n == 1:
-        #     return 5
-        # if n%2 == 1:
-        #     return 5*self.countGoodNumbers(n-1)%(pow(10, 9) + 7)
-        # else:
-        #     return 4*self.countGoodNumbers(n-1)%(pow(10, 9) + 7)
 
-        even = ceil(n/2)
+        
+        even = n//2 if n%2 == 0 else n//2 +1
         odd = n//2
-        mod = pow(10, 9) + 7
 
-        return pow(5, even, mod)*pow(4, odd, mod)%mod
-    
+        return pow(5, even, 1000000007)*pow(4, odd, 1000000007)%1000000007
+        
