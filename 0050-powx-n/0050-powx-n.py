1d0ef4ad8b6585ee8e5419b15d16689050e71005
Firaol kefeni <firaolkef@gmail.com>
2026-03-17 18:23:42 +0300

    Time: 0 ms (100%), Space: 19.6 MB (17.86%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -2,7 +2,22 @@ from math import pow
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-       return pow(x, n)
+        def solve(n, x):
+
+            if n == 1:
+                return x
+            if n == 0:
+                return 1
+            cur = solve(n//2, x)
+
+            return cur*cur if n%2 == 0 else x*cur*cur
+
+        val = solve(abs(n), x)
+
+        return val if n >= 0 else 1/val
+
+
+        
 
 
 
