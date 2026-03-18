9999051bf48d1ff4779da13acf3e677d2eeb1e4e
Firaol kefeni <firaolkef@gmail.com>
2026-03-18 18:22:02 +0300

    Time: 0 ms (100%), Space: 19.1 MB (97.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -5,37 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-   
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        arr = []
         
-        def dfs(node, visted=set()):
-            
+        res = []
+
+        def dfs(node):
+
             if not node:
-                return 
+                return
             
-            nonlocal arr
-            
-            left = node.left
-            right = node.right
-            visted.add(node)
+            nonlocal res
 
-            if left:
-                if left not in visted:
-                    dfs(left, visted)
-
-            arr.append(node.val)
-
-            if right:
-                if right not in visted:
-                    dfs(right, visted)
-                        
-            
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
         
         dfs(root)
 
-        return arr 
-        
-     
-        
+        return res
