8a188374b3a89ca1f6ca01056cc73d6861df42d8
Firaol kefeni <firaolkef@gmail.com>
2026-03-19 17:50:19 +0300

    Add solution for Two City Scheduling

---
+++ leetcode/1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        costs.sort( key=lambda x: x[0] -x[1])
+        total = 0
+        n = len(costs)
+        # print(costs)
+        for i in range(n//2):
+
+            total += costs[i][0] + costs[n-i-1][1]
+        
+        return total
