22feb694bcc629dc6da0e61c7d2682aa254be56c
Firaol kefeni <firaolkef@gmail.com>
2026-03-19 12:55:52 +0300

    Add solution for Find Consecutive Integers from a Data Stream

---
+++ leetcode/2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
@@ -0,0 +1,31 @@
+class DataStream:
+
+    def __init__(self, value: int, k: int):
+        self.queue = deque()
+        self.count = 0
+        self.value = value
+        self.k = k
+
+    def consec(self, num: int) -> bool:
+
+        if num == self.value:
+            self.count += 1
+        
+        self.queue.append(num)
+        
+        if len(self.queue) > self.k:
+            val = self.queue.popleft()
+
+            if val == self.value:
+                self.count -= 1
+        
+        if self.count == self.k:
+            return True
+        
+        return False
+            
+
+
+# Your DataStream object will be instantiated and called as such:
+# obj = DataStream(value, k)
+# param_1 = obj.consec(num)
