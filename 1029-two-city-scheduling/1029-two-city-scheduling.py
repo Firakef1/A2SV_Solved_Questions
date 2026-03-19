9f772f076b5629b236f21f707bad664205d33079
Firaol kefeni <firaolkef@gmail.com>
2026-03-19 17:50:11 +0300

    Time: 1 ms (22.3%), Space: 19.2 MB (86.2%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -4,7 +4,7 @@ class Solution:
         costs.sort( key=lambda x: x[0] -x[1])
         total = 0
         n = len(costs)
-        print(costs)
+        # print(costs)
         for i in range(n//2):
 
             total += costs[i][0] + costs[n-i-1][1]
