1bc1f7580c342ee58f0b0604ecb984357e1f4bf3
Firaol kefeni <firaolkef@gmail.com>
2026-03-24 19:19:19 +0300

    Add solution for Combinations

--- leetcode/0077-combinations/0077-combinations.py
+++ leetcode/0077-combinations/0077-combinations.py
@@ -1,20 +1,22 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
-        arr = []
-
-        def find(j, k, nums=[]):
-
-            if len(nums) == k:
-                nonlocal arr
-                arr.append(nums[:])
-                return
+        arr = [x for x in range(1, n + 1)]
+        nums = []
+        def combination(lst, i, curr):
+            nonlocal nums
+            if i == len(arr):
+                nums.append(curr)
+                return 
             
-            for i in range(j,n+1):
-                nums.append(i)
-                find(i+1, k, nums)
-                nums.pop()
-            
-        find(1, k)
 
-        return arr
+      
+            combination(lst, i + 1, curr + [lst[i]])
+
+            combination(lst, i + 1, curr)
+            
+
+
+        combination(arr, 0, [])
+        ans = [x for x in nums if len(x) == k]
+        return ans
