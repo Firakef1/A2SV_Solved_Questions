9d3b6754ccbeeceb42f5501f659604fd5ba17e73
Firaol kefeni <firaolkef@gmail.com>
2026-03-25 18:10:18 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -1,7 +1,19 @@
-from itertools import permutations
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-
-        return list(permutations(nums))
         
+        def find(lis):
+            if len(lis) <= 1:
+                return [lis]
+            
+            result = []
+            for i in range(len(lis)):
+                cur = lis[i]
+                
+                others = lis[:i] + lis[i+1:]
+                
+                for p in find(others):
+                    result.append([cur] + p)
+                    
+            return result
+        
+        return find(nums)
