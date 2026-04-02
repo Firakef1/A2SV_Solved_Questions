4c80e73d9c05771d6fce64d603e19bed191e7959
Firaol kefeni <firaolkef@gmail.com>
2026-04-02 09:40:56 +0300

    Add solution for Additive Number

--- leetcode/0306-additive-number/0306-additive-number.py
+++ leetcode/0306-additive-number/0306-additive-number.py
@@ -1,40 +1,41 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        n = len(num)
-
-        def isValid(s):
-            if len(s) > 1 and s[0] == '0':
-                return False
-            return True
 
         def find(first, second, index):
-            if index == n:
+            
+            if index == len(num):
                 return True
-            
-            for i in range(index + 1, n + 1):
-                val_str = num[index:i]
-                if not isValid(val_str):
-                    continue
-                
-                val = int(val_str)
-
-                if val == first + second:
-                    return find(second, val, i)
-                
-                if val > first + second:
+            val = 0
+            for i in range(index+1, len(num)+1):
+                val = num[index:i]
+                # print(val)
+                if val.startswith('0') and len(val) > 1:
                     break
-            
+                val = int(val)
+                if val == first+second:
+                    if find(second, val, i):
+                        return True
+            # print(val, "fianl")
             return False
-
-        for i in range(1, n):
-            s1 = num[:i]
-            if not isValid(s1): break 
             
-            for j in range(i + 1, n):
-                s2 = num[i:j]
-                if not isValid(s2): break 
+        for i in range(1, len(num)):
+            
+            val_one = num[:i]
+
+            if val_one.startswith('0') and len(val_one) > 1:
+                    break
+            val_one = int(val_one)
+
+            for j in range(i+1, len(num)):
                 
-                if find(int(s1), int(s2), j):
+                val_two = num[i:j]
+
+                if val_two.startswith('0') and len(val_two) > 1:
+                    break
+                
+                val_two = int(val_two)
+                # print(val_one, val_two)
+                if find(val_one, val_two, j):
                     return True
         
         return False
