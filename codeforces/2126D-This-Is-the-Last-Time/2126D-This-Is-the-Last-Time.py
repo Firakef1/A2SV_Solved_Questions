9e789bed53c0aebc72c14c4e58ebf844b2d2a45a
Firaol kefeni <firaolkef@gmail.com>
2026-04-09 20:08:29 +0300

    correct code

--- codeforces/2126D-This-Is-the-Last-Time/2126D-This-Is-the-Last-Time.py
+++ codeforces/2126D-This-Is-the-Last-Time/2126D-This-Is-the-Last-Time.py
@@ -1,4 +1,17 @@
-# print(casinos)
+t = int(input())
+
+for _ in range(t):
+    n, k = [int(x) for x in input().split()]
+
+    casinos = []
+
+    for _ in range(n):
+        casino = [int(x) for x in input().split()]
+        casinos.append(casino)
+
+    casinos.sort()
+
+    # print(casinos)
 
     for casino in casinos:
        if casino[2] > k and casino[0] <= k:
