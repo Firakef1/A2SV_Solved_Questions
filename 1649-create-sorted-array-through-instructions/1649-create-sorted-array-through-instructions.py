d09ca8b47e0ccc7a9757f5bcb14443d091129458
Firaol kefeni <firaolkef@gmail.com>
2026-04-20 19:37:19 +0300

    Time: 9310 ms (5.1%), Space: 33.4 MB (82.8%) - LeetHub

---
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -0,0 +1,14 @@
+class Solution:
+    def createSortedArray(self, instructions: List[int]) -> int:
+        nums = []
+        cost = 0
+
+        for inst in instructions:
+            entry = bisect_left(nums,inst)
+            end = bisect_right(nums,inst)
+
+            cost += min(entry, len(nums) - end)
+            nums.insert(entry,inst)
+            # print(nums)
+        
+        return cost % (10**9 + 7)
